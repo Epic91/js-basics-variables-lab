174c407da1984c991893b4d12343957bc1a4398a
Epic91 <hernandeze791@gmail.com>
2021-01-31 15:25:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+//expect(companyName).to.equal('Scuber');
+//let companyName = 'Scuber';
+
+//expect(js).to.match(/const companyName/
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
